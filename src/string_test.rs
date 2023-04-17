abc2c1a61ebdf00de533ccf6e4a819d543ab037a
bjs007 <bjs9527@163.com>
2023-04-17 21:44:17 +0800

    1

--- src/string_test.rs
+++ src/string_test.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 
 mod tests {
+    use std::ops::Add;
+
     use super::*;
     #[test]
     fn test() {
@@ -8,5 +10,12 @@ mod tests {
         let h = String::from("world");
         let a = s + &h;
         assert_eq!(a, "helloworld");
+
+        let mut s = String::from("hello");
+        s.push_str(" world");
+        s.push('!');
+        assert_eq!(s, "hello world!");
+        s = s.add("!!");
+        println!("{:?}", s);
     }
 }
