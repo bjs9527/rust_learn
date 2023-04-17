7003eb9be734f742824f543f024473656f6ac2d9
bjs007 <bjs9527@163.com>
2023-04-17 18:36:04 +0800

    1

--- src/match_test.rs
+++ src/match_test.rs
@@ -1,22 +1,30 @@
+pub enum UsState {
+    Alabama,
+    Alaska,
+}
 pub enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
-#[cfg(test)]
-pub mod tests{
-    use crate::match_test::Coin;
 
-  #[test]
-  fn test(){
-    let coin = Coin::Dime;
-    let value=match coin {
-      Coin::Penny => 1,
-      Coin::Nickel => 5,
-      Coin::Dime => 10,
-      Coin::Quarter => 25,        
-    };
-    assert_eq!(value,10);
-  }
+#[cfg(test)]
+pub mod tests {
+    use crate::match_test::{Coin, UsState};
+
+    #[test]
+    fn test() {
+        let coin = Coin::Quarter(UsState::Alaska);
+        let value = match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => match state {
+                UsState::Alabama => 26,
+                UsState::Alaska => 27,
+            },
+        };
+        assert_eq!(value, 27);
+    }
 }
