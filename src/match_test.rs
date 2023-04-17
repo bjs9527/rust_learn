2f00668cb5f45a933ead52c5fbd8e209504e520b
bjs007 <bjs9527@163.com>
2023-04-17 19:37:55 +0800

    match

--- src/match_test.rs
+++ src/match_test.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub enum UsState {
     Alabama,
     Alaska,
@@ -9,9 +10,19 @@ pub enum Coin {
     Quarter(UsState),
 }
 
+#[allow(dead_code)]
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(a) => Some(a + 1),
+        None => None,
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
     use crate::match_test::{Coin, UsState};
+
+    use super::plus_one;
 
     #[test]
     fn test() {
@@ -27,4 +38,26 @@ pub mod tests {
         };
         assert_eq!(value, 27);
     }
+
+    #[test]
+    fn test2() {
+        let a = Some(1);
+        let b = plus_one(a);
+        assert_eq!(b, Some(2));
+        let c = None;
+        let d = plus_one(c);
+        assert_eq!(d, None);
+    }
+}
+
+#[test]
+fn test3() {
+    fn a(x: i32) -> bool {
+        match x {
+            1 => x % 2 == 0,
+            2 => x % 2 == 0,
+            _ => false,
+        }
+    }
+    assert_eq!(a(2), true);
 }
